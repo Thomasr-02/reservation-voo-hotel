237ed9ae1313750585c07a73502dfc0a00f52fe0
Thomas Ribeiro <thomas10191@hotmail.com>
2020-04-14 12:56:15 -0300

    Add archive pop database, and config repository hotel

--- hotel/hotel/src/main/java/com/synchro/hotel/hotel/repository/hotelRepository.java
+++ hotel/hotel/src/main/java/com/synchro/hotel/hotel/repository/hotelRepository.java
@@ -8,8 +8,13 @@ import org.springframework.data.repository.CrudRepository;
 
 
 public interface hotelRepository extends CrudRepository<hotel, Integer>{
-   /*  @Query(value = "SELECT u FROM voos where u.cidade=?1")
-    ListFilter Iterable<hotel> findByEmployeeName(String cidade);       */
+  //  @QUERY("SELECT U FROM U.CIDADE WHERE U.CIDADE= CIDADE")
+ //   LISTFILTER LIST<HOTEL> FINDBYCITY(STRING CIDADE){
+
+    } 
+
+    /*  @Query(value = "SELECT u FROM voos where u.cidade=?1")
+    
+   ListFilter Iterable<hotel> findByEmployeeName(String cidade);       */
 //CRIAR FUNCAO PARA LISTAR POR NOME DE CIDADE
 //TROCAR POST POR GET EM FILTRAGEM DE VOOS
-}
---
+++ popDatabase.sql
@@ -0,0 +1,9 @@
+INSERT INTO `voo`(`nomeEmp`, `preco`, `origem`, `destino`, `data_ida`, `data_volta`) VALUES ("GOL",199.99,"Recife","Fortaleza","2020-04-15","2020-04-18");
+INSERT INTO `voo`(`nomeEmp`, `preco`, `origem`, `destino`, `data_ida`, `data_volta`) VALUES ("LATAM",239.99,"Recife","Máceio","2020-04-18","2020-05-18");
+INSERT INTO `voo`(`nomeEmp`, `preco`, `origem`, `destino`, `data_ida`, `data_volta`) VALUES ("LATAM",129.99,"Recife","João Pessoa","2020-04-16","2020-05-18");
+INSERT INTO `hotel`(`cidade`, `descricao`, `nome`, `preco`, `url_imagem`) VALUES ('Fortaleza',"Quartos chiques e casuais, a maioria com vista para o mar, com piscina, restaurante e café da manhã gratuito.
+","Porto Futuro Hotel Fortaleza",109,"https://lh3.googleusercontent.com/p/AF1QipOXULP9HlT-prbvNo__WNCne0on2Qo6yJA6kX9L=w296-h202-n-k-rw-no-v1");
+
+INSERT INTO `hotel`(`cidade`, `descricao`, `nome`, `preco`, `url_imagem`) VALUES ('Maceió',"Suítes estilosas em hospedagem sofisticada que tem piscina com vista para o mar, restaurante, bar e parquinho.
+","Maceió Mar Hotel",218,"https://lh3.googleusercontent.com/proxy/Z6wZ2UR0ggo8lvDx54M0HInNDhixPvTi21MUJkF0erdEbmhOB0kNB3568M0OzErzE4TAYSyHqrKGkYzKSI5wkV6cbWn27FAMPgc3211nXqKRVgEqsL4ALDZZWcLEr4BYtyQM6E46By3rkixH-WDpJr8wzPrBh7o=w296-h202-n-k-rw-no-v1");
+
